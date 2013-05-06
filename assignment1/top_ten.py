0f758df1ae3465e2e5b4fcf25b2522e9eca815ca
boplusplus <bo.chen.cs@gmail.com>
2013-05-06 22:59:32 +0100

    06/05

--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -1,54 +1,30 @@
-import sys
+﻿import sys
 import json
 from collections import Counter
-
-
-
-def find_state(dic, tweet_file):
-    hash_state = Counter()
-    for line in tweet_file:
-        t = json.loads(line)
-        if "text" in t:
-            place = t["place"]
-            if place != None:
-                if place["country_code"] == "US":
-                    state = place["full_name"][-2:]
-                    score = get_score(t, dic)
-                    if state in hash_state:
-                        hash_state[state] += score
-                    else:
-                        hash_state[state] = score
-    if len(hash_state) == 0:
-        print "none"
-    results = hash_state.most_common(10)
-    for result in results:
-        print result[0], result[1]
-    
-        
-                    
+       
 def get_tags(tweet_file):
-    hash_tag = {}
+    hash_tag = Counter()
     for line in tweet_file:
         t = json.loads(line)
         if "entities" in t:
             entity = t["entities"]
-            tag = entity["hashtags"]
-            if tag != []:
-                text = tag[0]["text"]
-                if text in hash_tag:
-                    hash_tag[text] += 1.0
-                else:
-                    hash_tag[text] = 1.0
+            tags = entity["hashtags"]
+            if tags != []:
+                for tag in tags:
+                    text = tag["text"]
+                    if text in hash_tag:
+                        hash_tag[text] += 1.0
+                    else:
+                        hash_tag[text] = 1.0
     #print text
-    if len(hash_tag) == 0:
-        print "none"
-    max = -100000
-    hap = ""
-    for text in hash_tag:
-        if hash_tag[text] > max:
-            max = hash_tag[text]
-            hap = text
-    print hap
+    # if len(hash_tag) == 0:
+        # print "none"
+    
+    results = hash_tag.most_common(10)
+    #print len(results), type(results)
+    for result in results:
+        key = result[0].encode('utf-8')
+        print key, result[1]
             
     
 def main():
@@ -56,12 +32,6 @@ def main():
     get_tags(tweet_file)
     tweet_file.close()
  
-
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
